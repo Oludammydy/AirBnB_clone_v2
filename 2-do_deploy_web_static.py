d1828c48f9b06f717f88757f91e94772923f5913
Oludammydy <oludammydy@gmail.com>
2022-09-30 11:04:47 -0700

    Deploy archive fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['44.201.98.252', '3.236.120.0']
+env.hosts = ['18.207.241.255', '3.235.243.226']
 
 
 def do_deploy(archive_path):
